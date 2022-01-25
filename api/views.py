c6dadf78bc4f7798e7715ab64bb27154caf1f643
elvis <elvissierra3@gmail.com>
2022-01-24 23:05:56 -0600

    .

--- api/views.py
+++ api/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-From .serializers import UserSerializer, GroupSerializer
+from .serializers import UserSerializer, GroupSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = User.objects.all().order_by('-date_joined')
+
+    queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
 
 
@@ -16,5 +17,6 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
+
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
