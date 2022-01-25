bb7e7060cdca0f2ad8dd6f28af5941721e7a4f2f
elvis <elvissierra3@gmail.com>
2022-01-24 23:26:04 -0600

    .

--- rapidapipractice/settings.py
+++ rapidapipractice/settings.py
@@ -121,4 +121,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 
-DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
